39a63ce454185a2b74dcea2193e635d5084d6cb4
Dmitry Orlov <d.orlov777@gmail.com>
2025-02-28 11:58:11 +0300

    feat: image test

--- e2e/features/first.feature
+++ e2e/features/first.feature
@@ -3,3 +3,12 @@ Feature: Test workflow
     Scenario: Open catalog and verify that the address, descriptions, main image, and thrumbnails are displayed
         Given Goto page "looks/w23"
         Then Address block has the "Balmain" brand
+
+    Scenario: Open catalog and verify that clicking the Next button changes both: the main image and the thrumbnail
+        Given Goto page "looks/w23"
+        Given Store "main-image" to "mainImage"
+        Given Store "thrumbnail-image" to "thrumbnailImage"
+        When Click the "next" button
+        When Wait for 3 sec
+        Then The "main-image" is not equal "mainImage"
+        Then The "thrumbnail-image" is not equal "thrumbnailImage"
--- e2e/step-definitions/page-steps.ts
+++ e2e/step-definitions/page-steps.ts
@@ -1,21 +1,91 @@
-import { Given, Then, When } from '@cucumber/cucumber'
-import { CustomWorld } from '../cucumber-setup/custom-world'
-import { setTimeout } from 'node:timers/promises'
+import { Given, Then, When } from "@cucumber/cucumber";
+import { CustomWorld } from "../cucumber-setup/custom-world";
+import { setTimeout } from "node:timers/promises";
+import * as assert from "node:assert";
 
-Given('Goto page {string}', async function (this: CustomWorld, path: string) {
-  this.page = await this.browser.newPage()
-  this.page.setViewportSize({ width: 1920, height: 1040 })
-  await this.page.goto(`${this.parameters.appUrl}/${path}`)
-})
+Given("Goto page {string}", async function (this: CustomWorld, path: string) {
+  this.page = await this.browser.newPage();
+  this.page.setViewportSize({ width: 1920, height: 1040 });
+  await this.page.goto(`${this.parameters.appUrl}/${path}`);
+});
 
-Then('Address block has the {string} brand', async function (this: CustomWorld, brand: string) {
-  await this.page.locator('.catalog__address').getByText(brand).waitFor()
-})
+Then(
+  "Address block has the {string} brand",
+  async function (this: CustomWorld, brand: string) {
+    await this.page.locator(".catalog__address").getByText(brand).waitFor();
+  }
+);
 
-Given('Wait for {int} sec', async function (sec: number) {
-  await setTimeout(sec * 1000)
-})
+Given("Wait for {int} sec", async function (sec: number) {
+  await setTimeout(sec * 1000);
+});
 
-When('Screenshot {string}', async function (this: CustomWorld, name: string) {
-  await this.screenshot(name)
-})
+When("Screenshot {string}", async function (this: CustomWorld, name: string) {
+  await this.screenshot(name);
+});
+
+Given(
+  "Store {string} to {string}",
+  async function (this: CustomWorld, element: string, store: string) {
+    let elementData: string;
+    switch (element) {
+      case "main-image":
+        elementData = await this.page
+          .locator(".slide--active .look__image")
+          .evaluate((el) => {
+            return el.getAttribute("src");
+          });
+        break;
+
+      case "thrumbnail-image":
+        elementData = await this.page
+          .locator(".nav-carousel.navigation .slide--active img")
+          .evaluate((el) => {
+            return el.getAttribute("src");
+          });
+        break;
+    }
+
+    this.scope.set(store, elementData);
+  }
+);
+
+When(
+  "Click the {string} button",
+  async function (this: CustomWorld, button: string) {
+    switch (button) {
+      case "next":
+        await this.page
+          .locator(".catalog__look-navigation button")
+          .last()
+          .click();
+        break;
+    }
+  }
+);
+
+Then(
+  "The {string} is not equal {string}",
+  async function (this: CustomWorld, element: string, store: string) {
+    let elementData: string;
+    switch (element) {
+      case "main-image":
+        elementData = await this.page
+          .locator(".look-navigation .slide--active .look__image")
+          .evaluate((el) => {
+            return el.getAttribute("src");
+          });
+        break;
+
+      case "thrumbnail-image":
+        elementData = await this.page
+          .locator(".nav-carousel.navigation .slide--active img")
+          .evaluate((el) => {
+            return el.getAttribute("src");
+          });
+        break;
+    }
+
+    assert.notEqual(elementData, this.scope.get(store));
+  }
+);
